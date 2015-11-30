8f27ffa7ece8444ddc17b318b5b2536e52a8af96
Immunity-Project <gkm24@drexel.edu>
2015-11-30 17:33:31 -0500

    almost done with 2048 game
    Almost there

--- Python/2048Game/Game2048.py
+++ Python/2048Game/Game2048.py
@@ -16,6 +16,27 @@ BLUE = (  0,   0, 255)
 
 KeyMovement = "NONE";
 OldMovement = "NONE";
+SomethingHappened = True
+xMult = 0;
+yMult = 0;
+
+
+# initialize font; must be called after 'pygame.init()' to avoid 'Font not Initialized' error
+myfont = pygame.font.SysFont("monospace", 16)
+
+
+
+class Square:
+    def __init__(self, xLoc, yLoc, isTrue, value):
+        self.xLoc = xLoc
+        self.yLoc = yLoc
+        self.isTrue = isTrue
+        self.value = value;
+        self.label = myfont.render(str(self.value), 1, (255,255,0))
+        self.rect = pygame.Rect(xLoc,yLoc,50,50)
+
+        
+
 
 # create squares and attach values to it
 VisibleSquare = [];
@@ -24,11 +45,12 @@ spacing = 60;
 for i in range(4):   
     for j in range(4):
         if random.randint(0,4) == 2:
-           VisibleSquare.append(pygame.draw.rect(DISPLAYSURF, GREY, (0+(spacing*i), 0+(spacing*j), 50, 50)))
-           NumberValue.append(2);
+           VisibleSquare.append(Square(0+(spacing*i), 0+(spacing*j),True,2))
+           #NumberValue.append(2);
+           NumberValue.append("foosrowdah")
         else:
-            VisibleSquare.append(0)
-            NumberValue.append(0)
+           VisibleSquare.append(Square(0+(spacing*i), 0+(spacing*j),False,0))
+           NumberValue.append(0)
 
 print VisibleSquare
 print NumberValue
@@ -36,36 +58,97 @@ print NumberValue
 
 
 
+
+
 def PlayerMove():
-    if OldMovement == KeyMovement:
-        print "Same Move, Do Nothing!"
+        NoSpawn = False;
+        for i,obj in enumerate(VisibleSquare):
+            if VisibleSquare[i].isTrue:
+                pygame.draw.rect(DISPLAYSURF, RED, (VisibleSquare[i].xLoc+25 + (50*xMult), VisibleSquare[i].yLoc+25 + (50*yMult), 5, 5))
+                for j,sqr in enumerate(VisibleSquare):
+                    if VisibleSquare[j].rect.collidepoint(VisibleSquare[i].xLoc+25 + (50*xMult), VisibleSquare[i].yLoc+25 + (50*yMult)):
+                        if VisibleSquare[j].isTrue == False:
+                            print "moving!"
+                            VisibleSquare[j].isTrue = True;
+                            VisibleSquare[j].value = VisibleSquare[i].value;
+                            VisibleSquare[i].isTrue = False;
+                            SomethingHappened = True
+                            PlayerMove()
+                        else:
+                           if VisibleSquare[i].value == VisibleSquare[j].value:
+                              print "Same val, changing!"
+                              VisibleSquare[j].value += VisibleSquare[i].value
+                              VisibleSquare[i].isTrue = False;
+                              SomethingHappened = True
+                           else:
+                              print "Not same..."
+                              SomethingHappened = False
+
+
+                
+
+
+def SpawnRandom():
+    Selected = random.randint(0,15);
+    if VisibleSquare[Selected].isTrue == False:
+        VisibleSquare[Selected].value = 2;
+        VisibleSquare[Selected].isTrue = True;
+        print SomethingHappened;
     else:
-        print "Do it!"
-
-
-
+        try:
+            SpawnRandom();
+        except RuntimeError:
+            print "Game Over!"
+            pygame.quit()
+            sys.exit()
+            
+                
+    
 
 
 
 
 
 while True:
+    for i,obj in enumerate(VisibleSquare):
+        if VisibleSquare[i].isTrue == True:
+            pygame.draw.rect(DISPLAYSURF, GREY, (VisibleSquare[i].xLoc, VisibleSquare[i].yLoc, 50, 50))
+            VisibleSquare[i].label = myfont.render(str(VisibleSquare[i].value), 1, BLUE)
+            DISPLAYSURF.blit(VisibleSquare[i].label, (15+ VisibleSquare[i].xLoc, 15+ VisibleSquare[i].yLoc))
+        else:
+            pygame.draw.rect(DISPLAYSURF, WHITE, (VisibleSquare[i].xLoc, VisibleSquare[i].yLoc, 50, 50))
+
+
+
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             OldMovement = KeyMovement
             if event.key == pygame.K_LEFT:
                 KeyMovement = "LEFT"
+                xMult = -1;
+                yMult = 0;
                 print "LEFT"
             elif event.key == pygame.K_RIGHT:
                 KeyMovement = "RIGHT"
+                xMult = 1;
+                yMult = 0;
                 print "RIGHT"
             elif event.key == pygame.K_UP:
                 KeyMovement = "UP"
+                xMult = 0;
+                yMult = -1;
                 print "UP"
             elif event.key == pygame.K_DOWN:
                 KeyMovement = "DOWN"
+                xMult = 0;
+                yMult = 1;
                 print "DOWN"
+            elif event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                sys.exit()
             PlayerMove()
+            if KeyMovement != OldMovement:
+                SpawnRandom()
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
