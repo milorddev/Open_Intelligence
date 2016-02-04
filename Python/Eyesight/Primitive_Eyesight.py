16934c14972478f7737d5dd2e669f1228b9fca99
Immunity-Project <gkm24@drexel.edu>
2016-02-04 04:26:24 -0500

    open cv edge detection sturf
    I love my baby

--- Python/Eyesight/Primitive_Eyesight.py
+++ Python/Eyesight/Primitive_Eyesight.py
@@ -1,37 +1,87 @@
 import cv2
+import numpy as np
+
 
 WindowName = "Primitive Eyesight"
-Room_Width = 640;
-Room_Height = 480;
+Room_Width = 500;
+Room_Height = 400;
 
 
 cv2.namedWindow(WindowName)
 vc = cv2.VideoCapture(0)
 
+PaintList = [];
 
 if vc.isOpened(): # try to get the first frame
     rval, frame = vc.read()
     cv2.imshow(WindowName, frame)
+    cv2.imshow('GreyScale',frame);
+    
     cv2.resizeWindow(WindowName,Room_Width,Room_Height);
+    cv2.resizeWindow('GreyScale',Room_Width,Room_Height);
+
+
+    greyframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY);
+    b,g,r = cv2.split(frame)
+
+    #sobelx = cv2.Sobel(greyframe,cv2.CV_64F,1,0,ksize=5)
+    #sobely = cv2.Sobel(greyframe,cv2.CV_64F,0,1,ksize=5)
+    
+    #BlendSobel = cv2.addWeighted(sobelx,0.5,sobely,0.5,0);
+    
+    edges = cv2.Canny(greyframe,61,161, apertureSize = 7)
+    #lines = cv2.HoughLinesP(edges,1,np.pi/180,100,100,10)
+
+    BlendFrame = cv2.addWeighted(greyframe,0.5,edges,0.5,0);
+
+    
+
+    for i in range(0,200):
+        for j in range(0,200):
+            PaintList = BlendFrame[i,j];
+    
 else:
     rval = False
 
 
 
+
+
+
 while rval:
-    cv2.imshow(WindowName, frame)
     rval, frame = vc.read()
     key = cv2.waitKey(20)
     if key == 27: # exit on ESC
         break
 
-    frame = cv2.GaussianBlur(frame,(101,101),15)
+    greyframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY);
+    b,g,r = cv2.split(frame)
 
+    sobelx = cv2.Sobel(greyframe,cv2.CV_64F,1,0,ksize=5)
+    sobely = cv2.Sobel(greyframe,cv2.CV_64F,0,1,ksize=5)
+    
+    BlendSobel = cv2.addWeighted(sobelx,0.5,sobely,0.5,0);
+    
+    edges = cv2.Canny(greyframe,61,161, apertureSize = 7)
+    #lines = cv2.HoughLinesP(edges,1,np.pi/180,100,100,10)
+
+    #cv2.rectangle(frame,(0,0),(200,200),(212,212,212),3)
+
+    BlendFrame = cv2.addWeighted(greyframe,0.5,edges,0.5,0);
     
 
 
+    
+    cv2.imshow(WindowName, BlendSobel)    
+    cv2.imshow('GreyScale',BlendFrame);
+    
+   
 
+
+
+        
     
 vc.release()
 cv2.destroyWindow(WindowName)
+cv2.destroyWindow('GreyScale')
 
--- Python/Hearing/AmplitudeTest.py
+++ Python/Hearing/AmplitudeTest.py
@@ -41,13 +41,6 @@ UNDERSENSITIVE = 120.0/INPUT_BLOCK_TIME
 MAX_TAP_BLOCKS = 0.15/INPUT_BLOCK_TIME
 
 def get_rms( block ):
-    # RMS amplitude is defined as the square root of the 
-    # mean over time of the square of the amplitude.
-    # so we need to convert this string of bytes into 
-    # a string of 16-bit samples...
-
-    # we will get one short out for each 
-    # two chars in the string.
     count = len(block)/2
     format = "%dh"%(count)
     shorts = struct.unpack( format, block )
